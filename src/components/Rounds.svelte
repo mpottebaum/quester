206deff4d501d3fe2e6a50d153f2897e57a1aa5f
Mike Pottebaum <michael.pottebaum@cdw.com>
2023-12-17 16:30:47 -0600

    chore: add svelte

---
+++ src/components/Rounds.svelte
@@ -0,0 +1,19 @@
+<script lang='ts'>
+  export let rounds: Question[][];
+</script>
+
+<ol>
+  {#each rounds as round, i}
+    <li>
+      <h2>Round {i + 1}</h2>
+      <ol>
+        {#each round as question}
+          <li>{question.category}</li>
+        {/each}
+      </ol>
+    </li>
+  {/each}
+</ol>
+
+<style>
+</style>
